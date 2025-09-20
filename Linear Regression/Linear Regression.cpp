c4b38e948fe4f36aa55886c4e0e6fb20b2d6b3b2
Divyum Singhal <divyumsinghal@gmail.com>
2025-09-20 16:35:32 +0100

    Add project files.

---
+++ Linear Regression/Linear Regression.cpp
@@ -0,0 +1,12 @@
+﻿// Linear Regression.cpp : Defines the entry point for the application.
+//
+
+#include "Linear Regression.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
